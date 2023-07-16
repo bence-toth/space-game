5e8f517dd4c5198a96d69e9b874d460d1e7d2fc2
Bence A. Tóth <tothab@gmail.com>
2023-07-16 19:47:35 +0200

    Added highlight and level border animation

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 const root = document.querySelector(":root");
 
 window.addEventListener("mousemove", (mouseMoveEvent) => {
-  root.style.setProperty("--mouse-x", `${mouseMoveEvent.clientX}px`);
-  root.style.setProperty("--mouse-y", `${mouseMoveEvent.clientY}px`);
+  const viewportWidth = window.innerWidth;
+  const viewportHeight = window.innerHeight;
+  const mouseX = mouseMoveEvent.clientX;
+  const mouseY = mouseMoveEvent.clientY;
+  root.style.setProperty(
+    "--spotlight-center-x",
+    `${(100 * mouseX) / viewportWidth}%`
+  );
+  root.style.setProperty(
+    "--spotlight-center-y",
+    `${(100 * mouseY) / viewportHeight}%`
+  );
+  const xDistanceFromCenter = mouseX / viewportWidth - 0.5;
+  const yDistanceFromCenter = mouseY / viewportHeight - 0.5;
+  const distanceFromCenter =
+    2 *
+    ((xDistanceFromCenter ** 2 + yDistanceFromCenter ** 2) ** 0.5 / 2 ** 0.5);
+  console.log(distanceFromCenter);
+  root.style.setProperty(
+    "--spotlight-radius-inner",
+    `${(15 + distanceFromCenter * 10).toFixed(4)}%`
+  );
+  root.style.setProperty(
+    "--spotlight-radius-outer",
+    `${(25 + distanceFromCenter * 10).toFixed(4)}%`
+  );
 });
